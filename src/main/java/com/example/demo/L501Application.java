438be85a0fec7c56c5a19ff05dcb64fe10146793
cmchew <write_on@hotmail.com>
2020-01-09 17:28:20 -0500

    L501

---
+++ src/main/java/com/example/demo/L501Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L501Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L501Application.class, args);
+    }
+
+}
